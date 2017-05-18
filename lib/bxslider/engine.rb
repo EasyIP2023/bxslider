cdbc1f669c6c9ac39e257cb332f88c6d4935037e
phantomfreeJr <vdavis@siu.edu>
2017-05-18 18:20:49 -0500

    re add precompile

--- lib/bxslider/engine.rb
+++ lib/bxslider/engine.rb
@@ -3,6 +3,8 @@ module Bxslider
   class Engine < ::Rails::Engine
     initializer 'bxslider.assets.precompile' do |app|
       app.config.assets.precompile += %w(controls.png bx_loader.gif)
+      app.config.assets.precompile += %w(jquery.bxslider.js jquery.easing.1.3.js jquery.fitvids.js jquery.min.js)
+      app.config.assets.precompile += %w(jquery.bxslider.css)
     end
   end
 end
