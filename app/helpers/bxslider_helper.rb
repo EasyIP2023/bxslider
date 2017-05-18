22db5387258e42f39696472fe551cc943ad7a282
phantomfreeJr <vdavis@siu.edu>
2017-05-18 17:13:51 -0500

    remove precompile

--- app/helpers/bxslider_helper.rb
+++ app/helpers/bxslider_helper.rb
@@ -1,11 +1,6 @@
 module BxsliderHelper
-# Convenient helper to create a ul and li tag for your slider. The method's
-# definition is similar to Action View's `content_tag` helper.
-# It doesn't escape the given content by default though.
+
   def bx_slider(content_or_options = nil, options = {}, escape = false, &block)
-    # We are just keeping the two arguments to match Action View's
-    # definition of `content_tag` but the first argument isn't
-    # considered as the content if it's a Hash object.
     if content_or_options.kind_of?(Hash)
       options.merge!(content_or_options)
     end
